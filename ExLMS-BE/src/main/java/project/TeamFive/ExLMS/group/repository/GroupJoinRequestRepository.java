cb45a9ea7277d10c45b1d30afb0e1e951adfc6ad
MingFang914 <phuongvnmp@gmail.com>
2026-04-04 21:17:15 +0700

    hotfix: conflict handled

--- ExLMS-BE/src/main/java/project/TeamFive/ExLMS/group/repository/GroupJoinRequestRepository.java
+++ ExLMS-BE/src/main/java/project/TeamFive/ExLMS/group/repository/GroupJoinRequestRepository.java
@@ -1,6 +1,7 @@
 package project.TeamFive.ExLMS.group.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
